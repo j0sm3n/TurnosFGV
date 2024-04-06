0a84db6783c149c9f5ab6bc6b6d7ea2a936df40e
Jose Antonio Mendoza <josmen@gmail.com>
2024-04-06 08:00:36 +0200

    Remove unnecessary code

--- TurnosFGV/Extensions/Shift+Extensions.swift
+++ TurnosFGV/Extensions/Shift+Extensions.swift
@@ -12,34 +12,6 @@ extension Shift {
         startTime + duration
     }
     
-    private var startDate: Date {
-        Date.now.adjust(for: .startOfDay)!.addingTimeInterval(startTime)
-    }
-    
-    private var endDate: Date {
-        startDate.addingTimeInterval(duration)
-    }
-    
-    var viewStartString: String {
-        startDate.formatted(date: .omitted, time: .shortened)
-    }
-    
-    var viewEndString: String {
-        startDate.addingTimeInterval(duration).formatted(date: .omitted, time: .shortened)
-    }
-    
-    var shiftNightTime: TimeInterval {
-        Date().nightTime(startDate: startDate, endDate: endDate)
-    }
-    
-    var nightTimeString: String {
-        shiftNightTime.timeString
-    }
-    
-    var workingHours: String {
-        duration.timeString
-    }
-    
     var typeOfShift: TypeOfShift {
         let maxMorningStart = TimeInterval(hour: 12, minute: 30)
         let maxMorningEnd = TimeInterval(hour: 15, minute: 45)
