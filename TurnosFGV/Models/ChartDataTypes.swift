0700e95520fdc2b3c6f81147c8d886d74b1a2f7d
Jose Antonio Mendoza <66326890+j0sm3n@users.noreply.github.com>
2024-05-25 07:09:21 +0200

    refactor and improve charts

---
+++ TurnosFGV/Models/ChartDataTypes.swift
@@ -0,0 +1,26 @@
+//
+//  ChartDataTypes.swift
+//  TurnosFGV
+//
+//  Created by Jose Antonio Mendoza on 25/5/24.
+//
+
+import Foundation
+
+struct MonthChartData: Identifiable {
+    let id: UUID = .init()
+    let date: Date
+    let workedHours: Double
+    
+    // Animatable propertie
+    var isAnimated: Bool = false
+}
+
+struct TypeChartData: Identifiable {
+    let id: UUID = .init()
+    let type: String
+    let workedHours: Double
+    
+    // Animatable propertie
+    var isAnimated: Bool = false
+}
