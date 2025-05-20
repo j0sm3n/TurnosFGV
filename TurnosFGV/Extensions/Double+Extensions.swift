e1b606f23fa55f25517e3eb94a126c2d22d8cdea
Jose Antonio Mendoza <66326890+j0sm3n@users.noreply.github.com>
2025-05-20 11:58:34 +0200

    Fixes an issue where overtime could not be added when the end of the shift exceeded 23:59

--- TurnosFGV/Extensions/Double+Extensions.swift
+++ TurnosFGV/Extensions/Double+Extensions.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension Double {
     func roundToDecimal(_ fractionDigits: Int) -> Double {
-        let multiplier = pow(10, Double(fractionDigits))
+        let multiplier = Double.pow(10, Double(fractionDigits))
         return Darwin.round(self * multiplier) / multiplier
     }
 }
